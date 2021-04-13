6bf5e7745f91b2b1784d8e8d8fda0f682df2d3ae
Sabbir Ahamed <73008358+sabbir72@users.noreply.github.com>
2021-04-13 22:07:21 +0600

    Update index.php

--- web-lab/10 answer/index.php
+++ web-lab/10 answer/index.php
@@ -1,14 +1,28 @@
-<?php
-$color = array('white', 'green', 'red');
-foreach ($color as $c)
-{
-echo "$c, ";
-}
-sort($color);
-echo "<ul>";
-foreach ($color as $y)
-{
-echo "<li>$y</li>";
-}
-echo "</ul>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $color = array('white', 'green', 'red');
+    ?>
+    <ol type="A">
+    <li>
+        <?php for($i=0;$i<count($color);$i++){ 
+            echo $color[$i] . ", ";
+        } ?></li>
+        <li></li>
+        
+    </ol>
+
+    <ul>
+    <?php for($i=0;$i<count($color);$i++){ ?>
+        <li> <?php echo $color[$i] ?></li>
+        <?php } ?>
+    </ul> 
+</body>
+</html>
